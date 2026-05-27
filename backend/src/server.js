aa62613498f418e4761023cc5b3593d086211ce7
Manvitha593 <srimanvithalekkala@gmail.com>
2026-05-27 11:40:58 +0530

    Initial Project Setup

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Expense Manager API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
